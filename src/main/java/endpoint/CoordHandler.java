65557db73e2eeef9651217552765430271b03778
zocker-160 <zocker1600@posteo.net>
2022-09-13 16:58:44 +0200

    print timecode in log

--- src/main/java/endpoint/CoordHandler.java
+++ src/main/java/endpoint/CoordHandler.java
@@ -3,6 +3,7 @@ package endpoint;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import model.IPDatabase;
+import utils.Utils;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -11,7 +12,7 @@ public class CoordHandler implements HttpHandler {
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        System.out.println("got coord request");
+        System.out.println("["+ Utils.getTimecode()+"] got coord request");
 
         var is = exchange.getRequestBody();
         var request = new String(is.readAllBytes(), StandardCharsets.UTF_8);
